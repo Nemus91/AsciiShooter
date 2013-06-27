50a2df1699b8d6bf524a885de44dcadcdac2e596
Philipp <Philipp_goecke@web.de>
2013-06-27 13:39:25 +0200

    Frametest

--- Program.cs
+++ Program.cs
@@ -48,14 +48,37 @@ namespace AsciiShooter
                 Console.WindowWidth = Console.LargestWindowWidth;
 
             //Fill Array with empty spaces
+            //for (int y = 0; y < Map.GetLength(1); y++)
+            //{
+            //    for (int x = 0; x < Map.GetLength(0); x++)
+            //    {
+            //        Map[x, y] = (char)0;
+            //        Console.Write(Map[x, y]);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //Map Init
             for (int y = 0; y < Map.GetLength(1); y++)
             {
                 for (int x = 0; x < Map.GetLength(0); x++)
                 {
-                    Map[x, y] = (char)0;
-                    Console.Write(Map[x, y]);
+                    if (y == 0 || y == 49)
+                    {
+                        Map[x, y] = (char)45;
+                    }
+                    else
+                    {
+                        if (x == 0 || x == 99)
+                        {
+                            Map[x, y] = (char)124;
+                        }
+                        else
+                        {
+                            Map[x, y] = (char)0;
+                        }
+                    }
                 }
-                Console.WriteLine();
             }
         }
 
@@ -133,28 +156,39 @@ namespace AsciiShooter
         /// </summary>
         private static void UpdateMap()
         {
-            ChangeMap();
-
-            //TEST GEAR
-            // Gear g1 = new Gear();
-            // g1.Upgrade();
+            //ChangeMap();
         }
 
 
         //test methods
         static int z = 0;
         static Random a = new Random(20);
+        static int v = 0;
         private static void ChangeMap()
         {
-            z++;
-            if (z % 2 == 0)
+            z++;            
+            if (z % 4 == 0)
             {
-
+                v++;
                 int x = a.Next(sizeWidth);
                 int y = a.Next(sizeHeight);
                 int c = a.Next(20);
-                Map[x, y] = (char)(64+c);
-                System.Diagnostics.Debug.WriteLine(x + " " + y);
+                if (v < 49)
+                {
+                    Map[v - 1, v - 1] = (char)0;
+                    Map[v, v] = (char)(64);
+                }
+                if (v < 49)
+                {
+                    Map[2*v - 2, v - 1] = (char)0;
+                    Map[2*v, v] = (char)(64);
+                }
+                else
+                { 
+                    v = 0; 
+                }
+                System.Diagnostics.Debug.WriteLine(v);
+                Console.ReadKey();
             }
         }
     }
