40de421b708bc2925dc69dfe672b9963bd6e2a18
Kay Sofsky <kay_sofsky@web.de>
2013-08-01 12:42:14 +0200

    Bullet Class

---
+++ Entities/Bullet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AsciiShooter.Entities
+{
+    public class Bullet: MoveableObject
+    {
+        public override char VisRepresentation
+        {
+            get
+            {
+                return (char)11; 
+            }
+        }
+
+        public override void Update()
+        {
+            if (Position != (LastPosition))
+            {
+                Range --;
+            }
+            if (Range ==0)
+            {
+                Destroy();
+            }
+        }
+
+        public int Damage;
+        public int Range;
+    }
+}
