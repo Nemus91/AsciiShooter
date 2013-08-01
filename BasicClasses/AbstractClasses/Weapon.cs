956147f75b84c9b323e9197420dd438c3d2e52cb
Eve <evelyn.mason@gmx.de>
2013-08-01 12:40:52 +0200

    Weapon & Pistol class added

---
+++ BasicClasses/AbstractClasses/Weapon.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AsciiShooter.Entities
+{
+   
+    public abstract class Weapon : MoveableObject
+    {
+        public abstract int Damage
+        {
+            get;
+            set;
+        }
+
+        public abstract int Ammunition
+        {
+            get;
+            set;
+        }
+
+        public abstract int Range
+        {
+            get;
+            set;
+        }
+
+        public abstract int Firespeed
+        {
+            get;
+            set;
+        }
+
+        public abstract int Bulletspeed
+        {
+            get;
+            set;
+        }
+    }
+}
