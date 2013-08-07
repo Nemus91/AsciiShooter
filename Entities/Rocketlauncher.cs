938f2fa7ab0593341f83c5c6632d2fff8ea8b428
Eve <evelyn.mason@gmx.de>
2013-08-07 11:34:50 +0200

    added rocketlauncher

---
+++ Entities/Rocketlauncher.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AsciiShooter.Entities
+{
+    public class Pistol : Weapon
+    {
+        public Pistol()
+        {
+            Damage = 11;
+
+            Ammunition = 3;
+
+            Range = 14;
+
+            Firespeed = 2;
+
+            Bulletspeed = 16;
+
+        }
+
+        public override void Update()
+        {
+        }
+
+        public override char VisRepresentation
+        {
+            get { return (char)82; }
+        }
+
+        public override int Ammunition
+        {
+            get;
+            set;
+        }
+
+        public override int Damage
+        {
+            get;
+            set;
+
+        }
+
+        public override int Range
+        {
+            get;
+            set;
+
+        }
+
+        public override int Firespeed
+        {
+            get;
+            set;
+
+        }
+
+        public override int Bulletspeed
+        {
+            get;
+            set;
+
+        }
+    }
+}
